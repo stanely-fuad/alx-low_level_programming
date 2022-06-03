f7b2b506a605c67c3184f0866658476cd5e2fedc
stanely-fuad <stanelyfuad@gmail.com>
2022-06-03 04:09:43 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: random number in n +ve/-ve
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+
+int num, i;
+
+for (num = 48, i = 0; i < 10; num++, i++)
+
+{
+
+putchar(num);
+
+}
+
+putchar(10);
+
+return (0);
+
+}
