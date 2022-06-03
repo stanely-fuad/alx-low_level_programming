5b76b8c498c27bf0b43b8c23379a511543cd04bf
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-03 15:05:31 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-
 /**
  *main - entry point
  *Description: random number in n +ve/-ve
- *Return: Always 0 (success)
+ *Return: 0 on success
  */
-
 int main(void)
-
 {
-int n, k = 0, m;
-
-for (n = 0; n < 9; n++)
+int n, k = '0', m;
+for (n = '0'; n < '9'; n++)
 {
-for (m = k ; m <= 9; m++)
-}
+for (m = k ; m <= '9'; m++)
+{
 if (n != m)
 {
 putchar(n);
@@ -24,7 +20,7 @@ if (n == m)
 {
 continue;
 }
-if (n == 8 && m == 9)
+if (n == '8' && m == '9')
 {
 break;
 }
@@ -33,9 +29,9 @@ else
 putchar(',');
 putchar(' ');
 }
-{
+}
 k++;
 }
-putchar(n);
+putchar('\n');
 return (0);
 }
