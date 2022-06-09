3d77ed50bb7e883573938b7d85e10fcd37a99f1b
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-09 22:13:23 +0300

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,16 +7,18 @@
  */
 int main(void)
 {
+
 unsigned int long n = 612852475143, a = (int) sqrt(n);
 
 while (1)
 {
-if (n % a == 0)
-{
-printf("%lu \n", n / a);
-break;
-}
-a--;
+
+	if (n % a == 0)
+	{
+		printf("%lu \n", n / a);
+		break;
+	}
+	a--;
 
 }
 
