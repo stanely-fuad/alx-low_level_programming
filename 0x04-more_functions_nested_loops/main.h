2a5efd6f1c7605baf7722b05800357c3156973e0
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-09 18:18:41 +0300

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,16 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * *main - header file for protoypes
- * *void protoypes (void)
- * *int protoypes (int)
- * */
-<<<<<<< HEAD
-void print_numbers(void);
-int _isdigit(int c);
-=======
+ *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
 int _isupper(int c);
+int _isdigit(int c);
 
 #endif /* MAIN_H*/
-
->>>>>>> 559ac89a308bc9162965837ea04cbf6ad7ed83d0
