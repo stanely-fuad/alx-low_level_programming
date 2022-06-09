5002ffb7fc8b846eb193272faa2aa14b8a4f9845
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-09 20:54:14 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,9 +9,9 @@ void print_numbers(void)
 {
 int a;
 
-	for (a = 0; a <= 9; a++)
-	{
-		_putchar('0' + a);
-	}
-	_putchar('\n');
+for (a = 0; a <= 9; a++)
+{
+_putchar('0' + a);
+}
+_putchar('\n');
 }
