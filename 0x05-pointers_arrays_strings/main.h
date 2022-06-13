ed36a91654e1b636c14631fbec71abf732c73383
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-13 19:25:25 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,10 +8,10 @@
 * int prototypes()
 */
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-void swap_int(int *a, int *b);
 void swap_char(char *a, char *b);
 void puts2(char *str);
 void puts_half(char *str);
