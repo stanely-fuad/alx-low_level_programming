dfc6e389cf28d343fcbecd80790ba2d5453623ef
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-13 18:52:56 +0300

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,1 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
-* void prototypes()
-*char prototypes()
-* int prototypes()
-*/
-
-void reset_to_98(int *n); 
-
-#endif /* MAIN_H */
+void reset_to_98(int *n);
