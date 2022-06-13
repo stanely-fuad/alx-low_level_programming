e6e9f06f501437b9fa23fdb42b1667e5a8ffcdc0
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-13 17:59:30 +0300

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
-#include "main.h"
-/**
- * reset_to_98 - reset argument to 98
- * @n: pointer
- * Return: void
-*/
-void reset_to_98(int *n)
-{
-*n = 98;
-}
+#include "main.h" 
+  
+ /** 
+  * reset_to_98 - updates the value *n points to. 
+  * @n: the number to modify. 
+  */ 
+  
+ void reset_to_98(int *n) 
+ { 
+         *n = 98; 
+ }
