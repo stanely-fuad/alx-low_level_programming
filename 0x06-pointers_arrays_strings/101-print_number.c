4b3719478de5863dfdc9bba3527fc057adb3d9b7
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-16 20:15:22 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,21 +6,21 @@
    * Return: 0 
   **/ 
   
- void print_number(int n) 
- { 
-         unsigned int n1; 
+void print_number(int n) 
+{ 
+unsigned int n1; 
   
-         n1 = n; 
+n1 = n; 
   
-         if (n < 0) 
-         { 
-                 _putchar('-'); 
-                 n1 = -n; 
-         } 
+if (n < 0) 
+{ 
+_putchar('-'); 
+n1 = -n; 
+} 
   
-         if (n1 / 10 != 0) 
-         { 
-                 print_number(n1 / 10); 
-         } 
-         _putchar((n1 % 10) + '0'); 
- }
+if (n1 / 10 != 0) 
+{ 
+print_number(n1 / 10); 
+} 
+_putchar((n1 % 10) + '0'); 
+}
