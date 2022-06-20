1397fa735e9d2fecc141bb70f2477703f07b8a1b
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-20 15:08:26 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,22 +10,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int bytes = 0;
-  int i;
+unsigned int bytes = 0;
+int i;
 
-  while (*s)
-    {
-      for (i = 0; accept[i]; i++)
-	{
-	  if (accept[i] == *s)
-	    {
-	      bytes++;
-	      break;
-	    }
-	  else if ((accept[i + 1]) == '\0')
-	    return (bytes);
-	}
-      s++;
-    }
-  return (bytes);
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+{
+bytes++;
+break;
+}
+else if ((accept[i + 1]) == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
 }
