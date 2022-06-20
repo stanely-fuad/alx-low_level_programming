3541f3e2ed6cfa981a0dc94fefe85bbd9467b8d1
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-20 15:29:41 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,13 +7,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-int i, j;
+	int i, j;
 
-for (i = 0; a[i][7]; i++)
-{
-for (j = 0; j < 8; j++)
-_putchar(a[i][j]);
-_putchar('\n');
-}
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
 
 }
