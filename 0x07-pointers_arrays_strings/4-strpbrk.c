d5a9ebb440f9efbdcc75633676240bf8b822eb82
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-20 15:16:32 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,17 +13,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-  int i;
+int i;
 
-  while (*s)
-    {
-      for (i = 0; accept[i]; i++)
-	{
-	  if (accept[i] == *s)
-	    return (s);
-	}
-      s++;
-    }
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (accept[i] == *s)
+return (s);
+}
+s++;
+}
 
-  return ('\0');
+return ('\0');
 }
