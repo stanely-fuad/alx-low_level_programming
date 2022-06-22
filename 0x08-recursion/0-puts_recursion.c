688de8b817a6c2635513cb355d0fc27c23d8e102
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-22 18:37:36 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,13 +4,14 @@
  *@s:character to be printed
  *Return:returns void
  */
-void _puts_recursion(char *s)
-{
-if (*s == '\0')
-_putchar('\n');
-else
-{
-_putchar(s[0]);
-_puts_recursion(s + 1);
-}
+void _puts_recursion(char *s) 
+{ 
+if (s[0] == '\0') 
+{ 
+_putchar('\n'); 
+return; 
+} 
+  
+_putchar(s[0]); 
+_puts_recursion(s + 1); 
 }
