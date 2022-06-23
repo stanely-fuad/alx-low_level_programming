a5345c4ad8acccc1cfa2a98c5d5cd9f83f21d634
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-23 18:28:15 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,32 +1,28 @@
-#include "main.h"
-
-/**
-* _sqrt_recursion - returns the natural square root of a number
-* @n: number to be used
-*
-* Return: the square root of n
-*/
-
-int _sqrt_recursion(int n)
-{
-if (n == 0 || n == 1)
-return (n);
-return (_sqrt(0, n));
-}
-
-/**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
- *
- * Return: the square root of n
- */
-
-int _sqrt(int n, int x)
-{
-if (n > x / 2)
-return (-1);
-else if (n * n == x)
-return (n);
-return (_sqrt(n + 1, x));
-}
+#include "main.h" 
+  
+ /** 
+  * sqrt2 - Makes possible to evaluate from 1 to n 
+  * @a: same number as n 
+  * @b: number that iterates from 1 to n 
+  * Return: On success 1. 
+  * On error, -1 is returned, and errno is set appropriately. 
+  */ 
+  
+ int sqrt2(int a, int b) 
+ { 
+         if (b * b == a) 
+                 return (b); 
+         else if (b * b > a) 
+                 return (-1); 
+         return (sqrt2(a, b + 1)); 
+ } 
+ /** 
+  * _sqrt_recursion - returns the natural square root of n 
+  * @n: Number Integer 
+  * Return: On success 1. 
+  * On error, -1 is returned, and errno is set appropriately. 
+  */ 
+ int _sqrt_recursion(int n) 
+ { 
+         return (sqrt2(n, 1)); 
+ }
