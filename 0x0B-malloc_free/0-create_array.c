48512e2bd0cf213cb3935483d20e1469c1b35a17
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-29 18:32:42 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *array;
 unsigned int index;
 if (size == 0)
 return (NULL);
-array = malloc(sizeof(char) * size);
+array = (char *)malloc(sizeof(char) * size);
 if (array == NULL)
 return (NULL);
 for (index = 0; index < size; index++)
