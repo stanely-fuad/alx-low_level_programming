51799e8b622fc4869a70e126c4af2de5c83dc734
stanely-fuad <105486831+stanely-fuad@users.noreply.github.com>
2022-06-30 17:32:49 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,6 @@
-#include <stdio.h> 
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
 
 int _putchar(char ); 
@@ -7,3 +9,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size); 
 int *array_range(int min, int max); 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+
+#endif /* MAIN_H */
